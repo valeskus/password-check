cf77b7ae0513d2d2f9f35e86efdebb0a66b9f789
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-04-04 17:46:50 +0300

    Code optimized

--- src/components/ColorSection/ColorSection.jsx
+++ src/components/ColorSection/ColorSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './ColorSection.style.css'
 
 
-export function ColorSection({color}) {
+export function ColorSection({ color }) {
 
-    return (<div className={`indicator ${color}`}>
-
-    </div>)
+    return (
+        <div className="indicator" style={{backgroundColor: color}} />
+    )
 }
--- src/components/PasswordCheck/PasswordCheck.jsx
+++ src/components/PasswordCheck/PasswordCheck.jsx
@@ -3,26 +3,57 @@ import './PasswordCheck.style.css'
 import { ColorSection } from '../ColorSection/ColorSection';
 import { PasswordInput } from '../PasswordInput';
 
-export function PasswordCheck(props) {
+const letters = /[a-zA-Z]+/;
+const numbers = /[0-9]+/;
+const symbols = /[$-/:-?{-~!"^_`[\]]+/;
+const lettersAndNumbers = (v) => { return letters.test(v) && numbers.test(v) }
+const lettersAndSymbols = (v) => { return letters.test(v) && symbols.test(v) }
+const numbersAndSymbols = (v) => { return numbers.test(v) && symbols.test(v) }
+
+const LEVELS = {
+    NONE: "NONE",
+    LOW: "LOW",
+    MID: "MID",
+    HIGH: "HIGH"
+};
+
+const checkPasswordLevel = (password) => {
+    const isStrongPassword = letters.test(password) && numbers.test(password) && symbols.test(password);
+
+    const isMidPassword = lettersAndNumbers(password) || lettersAndSymbols(password) || numbersAndSymbols(password);
+
+    const isEasyPassword = letters.test(password) || numbers.test(password) || symbols.test(password);
+
+    if (isStrongPassword) {
+        return LEVELS.HIGH;
+    }
+
+    if (isMidPassword) {
+        return LEVELS.MID;
+    }
+
+    if (isEasyPassword) {
+        return LEVELS.LOW;
+    }
+}
+
+const LEVEL_COLOR = {
+    [LEVELS.NONE]: '#808080',
+    [LEVELS.LOW]: '#ff0000',
+    [LEVELS.MID]: '#ffdf00',
+    [LEVELS.HIGH]: '#0dce0d'
+};
+
+export function PasswordCheck() {
     const [value, setValue] = useState('');
-    const [easyColor, setEasyColor] = useState('');
-    const [mediumColor, setMediumColor] = useState('');
-    const [strongColor, setStrongColor] = useState('');
-
-    const letters = /[a-zA-Z]+/;
-    const numbers = /[0-9]+/;
-    const symbols = /[$-/:-?{-~!"^_`[\]]+/;
-    const lettersAndNumbers = (v) => { return letters.test(v) && numbers.test(v) }
-    const lettersAndSymbols = (v) => { return letters.test(v) && symbols.test(v) }
-    const numbersAndSymbols = (v) => { return numbers.test(v) && symbols.test(v) }
-
+    const [colors, setColors] = useState({
+        easy: LEVEL_COLOR.NONE,
+        medium: LEVEL_COLOR.NONE,
+        strong: LEVEL_COLOR.NONE,
+    });
 
     const handleChange = useCallback(
         (nextValue) => {
-            if (!nextValue) {
-                // props.onSearch(nextValue);
-            }
-
             setValue(nextValue);
             changeIndicator(nextValue)
         }, // eslint-disable-next-line
@@ -31,24 +62,42 @@ export function PasswordCheck(props) {
 
     const changeIndicator = useCallback(
         (nextValue) => {
-            if (letters.test(nextValue) && numbers.test(nextValue) && symbols.test(nextValue)) {
-                setEasyColor('greenColor')
-                setMediumColor('greenColor')
-                return setStrongColor('greenColor')
+            const passwordLevel = checkPasswordLevel(nextValue);
 
-            } else if (lettersAndNumbers(nextValue) || lettersAndSymbols(nextValue) || numbersAndSymbols(nextValue)) {
-                setStrongColor()
-                setEasyColor('yellowColor')
-                return setMediumColor('yellowColor')
-            } else if (letters.test(nextValue) || numbers.test(nextValue) || symbols.test(nextValue)) {
-                setMediumColor()
-                setStrongColor()
-                return setEasyColor('redColor')
+            switch (passwordLevel) {
+                case LEVELS.HIGH: {
+                    setColors({
+                        easy: LEVEL_COLOR.HIGH,
+                        medium: LEVEL_COLOR.HIGH,
+                        strong: LEVEL_COLOR.HIGH,
+                    });
+                    break;
+                }
+                case LEVELS.MID: {
+                    setColors({
+                        easy: LEVEL_COLOR.MID,
+                        medium: LEVEL_COLOR.MID,
+                        strong: LEVEL_COLOR.NONE,
+                    });
+                    break;
+                }
+                case LEVELS.LOW: {
+                    setColors({
+                        easy: LEVEL_COLOR.LOW,
+                        medium: LEVEL_COLOR.NONE,
+                        strong: LEVEL_COLOR.NONE,
+                    });
+                    break;
+                }
+                default: {
+                    setColors({
+                        easy: LEVEL_COLOR.NONE,
+                        medium: LEVEL_COLOR.NONE,
+                        strong: LEVEL_COLOR.NONE,
+                    });
+
+                }
             }
-            setEasyColor()
-            setMediumColor()
-            setStrongColor()
-
         }, // eslint-disable-next-line
         []
     );
@@ -60,9 +109,9 @@ export function PasswordCheck(props) {
                 value={value}
             />
             <div className='indicator-container'>
-                <ColorSection color={`${easyColor && easyColor}`} />
-                <ColorSection color={`${mediumColor && mediumColor}`} />
-                <ColorSection color={`${strongColor && strongColor}`} />
+                <ColorSection color={colors.easy} />
+                <ColorSection color={colors.medium} />
+                <ColorSection color={colors.strong} />
             </div>
 
         </div>
